c3801997811d7d4e583147466e59cf4f5421072c
JeffVenancius <jefferson.venancius@gmail.com>
2025-03-26 14:36:16 -0300

    finalizando

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-	base: process.env.VITE_BASE_PATH || "/JeffVenancius/otica-assis-24-2-25"
 })
